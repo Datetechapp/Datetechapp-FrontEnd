83da654a8359de9eb4d1142aac21733bb6e9b9c7
unknown <@DESKTOP-1POBAPR.sm.loc>
2023-06-07 12:30:45 +0300

    conflict-routs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,16 @@
-import React from "react"
+import { ModalAuth } from "./ModalAuth";
+import { ModalRegister } from "./pages/ModalRegister";
+
+import { Routes, Route } from "react-router-dom"
+
 
 export const App = () => {
   return <div>
+    <Routes>
+      <Route path="/auth" element={<ModalAuth />} />
+      <Route path="/registration" element={< ModalRegister/>} />
+
+    </Routes>
 
   </div>;
 };
-
