4d1ecbebb9389d58f7bbba036328f6e43a4650b4
unknown <@DESKTOP-1POBAPR.sm.loc>
2023-06-07 12:20:56 +0300

    conflict

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,8 @@
 import React from "react"
-import { ModalAuth } from "./ModalAuth/ModalAuth";
-import { ModalWelcome } from "./ModalWelcome/ModalWelcome";
-import { ModalWindAuth } from "./ModalWindAuth/ModalWindAuth";
-import img from "../assets/ModalAuth/questionSign.svg"
+
 export const App = () => {
   return <div>
-    <ModalAuth />
-    <ModalWelcome/>
-    <ModalWindAuth  image={img} text="We are glad to see you in the club" highlight="DateUpp"/>
+
   </div>;
 };
 
