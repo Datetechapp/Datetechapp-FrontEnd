261494ba39978adb4bb81d145701beb338614827
unknown <@DESKTOP-1POBAPR.sm.loc>
2023-06-19 14:36:05 +0300

    backgr and modal refact

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import './Header.css'
+import logo from "../../assets/ModalAuth/mod_welc-logo.svg"
+import { LanguagesSwich } from './LanguagesSwich'
+import { LanguageSelect } from './LanguageSelect/LanguageSelect'
+import { languages } from './language'
+import { BtnSignUp } from './BtnSignUp/BtnSignUp'
+export const Header = () =>{
+    return(
+        <header className='Header'>
+            <img src={logo} alt='logo'/>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Safety Tips</li>
+                    <li>Support</li>
+                </ul>  
+            </nav>
+            <LanguageSelect/>
+            <BtnSignUp/>
+            {/* <LanguagesSwich language={languages}/> */}
+        </header>
+    )
+}
