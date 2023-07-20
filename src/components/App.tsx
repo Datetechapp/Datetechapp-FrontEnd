cdf697ca666d7d284533f2903090be54ecd36315
unknown <@DESKTOP-1POBAPR.sm.loc>
2023-07-20 20:27:53 +0300

    save

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,15 @@
 import React from "react"
-import { ModalAuth } from "./ModalAuth";
-import { ModalRegister } from "./pages/ModalRegister";
-
+import { ModalAuth } from "./ModalAuth/ModalAuth";
+// import { ModalRegister } from "./pages/ModalRegister";
+import { BackSection } from "./BackSection/BackSection";
 import { Routes, Route } from "react-router-dom"
 
 
 export const App = () => {
   return <div>
-    <Routes>
-      <Route path="/auth" element={<ModalAuth />} />
-      <Route path="/registration" element={< ModalRegister/>} />
-
-    </Routes>
+   <BackSection>
+      <ModalAuth/>
+   </BackSection>
 
   </div>;
 };
