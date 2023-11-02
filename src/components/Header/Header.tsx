9903fc434d07f520acfc369548e17ccadbb6318d
taorminak <ostanina.anna@yahoo.com>
2023-11-02 16:11:55 +0100

    styles: fix styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,26 @@
-import styles from './Header.module.css'
-import logo from "../../assets/ModalAuth/mod_welc-logo.svg"
-import { LanguageSelect } from './LanguageSelect/LanguageSelect'
-import { BtnSignUp } from './BtnSignUp/BtnSignUp'
-
+import styles from "./Header.module.css";
+import logo from "../../assets/ModalAuth/mod_welc-logo.svg";
+import { LanguageSelect } from "./LanguageSelect/LanguageSelect";
+import { BtnSignUp } from "./BtnSignUp/BtnSignUp";
 
 export const Header = () => {
-    const navArr: string[] = ["Home", "About us", "Safety Tips", "Support"]
-    return (
-        <header className={styles.header}>
-            <img className={styles.header__logo} src={logo} alt='logo' />
-            <nav className={styles.header__nav}>
-                <ul className={styles.navList}>
-                    {navArr.map(link => <li className={styles.navListItem} key={link}>{link}</li>)}
-                </ul>
-            </nav>
-            <LanguageSelect />
-            <BtnSignUp />
-        </header>
-    )
-}
+  const navArr: string[] = ["Home", "About us", "Safety Tips", "Support"];
+  return (
+    <header className={styles.header}>
+      <img className={styles.header__logo} src={logo} alt="logo" />
+      <nav className={styles.header__nav}>
+        <ul className={styles.navList}>
+          {navArr.map((link) => (
+            <li className={styles.navListItem} key={link}>
+              {link}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className={styles.header__buttons}>
+        <LanguageSelect />
+        <BtnSignUp />
+      </div>
+    </header>
+  );
+};
