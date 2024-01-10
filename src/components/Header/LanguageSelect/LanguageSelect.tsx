23d7d8fe7559fb8e58794aa0f96d144d2d556827
daryapatsko <daryapatsko95@gmail.com>
2024-01-10 14:11:53 +0300

    feat: change languages menu

--- src/components/Header/LanguageSelect/LanguageSelect.tsx
+++ src/components/Header/LanguageSelect/LanguageSelect.tsx
@@ -2,34 +2,35 @@ import styles from './LanguageSelect.module.css';
 import '../LanguagesSwich.css';
 import { LanguagesSwich } from '../LanguagesSwich';
 import { languages } from '../language';
-import { useState, FC } from 'react';
+import { useState, FC, useEffect } from 'react';
 import { ReactComponent as ActiveArrow } from '../../../assets/Header/activeArrow.svg';
 import { ReactComponent as PassiveArrow } from '../../../assets/Header/passiveArrow.svg';
 
 export const LanguageSelect = () => {
-  const [activeVisible, setactiveVisible] = useState<string>('');
+  const [activeMenu, setActiveMenu] = useState<boolean>(false); 
   const [selectLang, setselectLang] = useState<string>('English');
 
-  const handleChangeVisibility = () => {
-    if (!activeVisible) {
-      setactiveVisible('Visible');
-    } else {
-      setactiveVisible('');
-    }
+  const handleActiveMenu = () =>{
+    setActiveMenu((prevState) => !prevState);
   };
 
   return (
     <div
-      onClick={() => handleChangeVisibility()}
+      onClick={handleActiveMenu }
       className={styles.LanguageSelect}
     >
       <p className={styles.selectLanguage}>{selectLang}</p>
-      {activeVisible ? <PassiveArrow /> : <ActiveArrow />}
-      <LanguagesSwich
-        ChangeLang={setselectLang}
-        nameClass={activeVisible}
-        language={languages}
-      />
+      {activeMenu ? <PassiveArrow /> : <ActiveArrow />}
+      {activeMenu && (
+        <div className={styles.LanguagesBox}>
+          {languages.map((language,index)=>(
+            <ul className={styles.LanguagesList}>
+              <li key={index} className={styles.LanguagesItem}>{language.lang}</li>
+            </ul>
+          ))}
+        </div>
+      )}
+      
     </div>
   );
 };
