178d3b146efd2591b55e9ed47309f5038576f367
daryapatsko <daryapatsko95@gmail.com>
2024-01-16 17:34:14 +0300

    fix: fix some mistake and change style of language menu

--- src/components/Header/LanguageSelect/LanguageSelect.tsx
+++ src/components/Header/LanguageSelect/LanguageSelect.tsx
@@ -1,39 +1,44 @@
 import React from 'react';
 import styles from './LanguageSelect.module.css';
 import '../LanguagesSwich.css';
-import { LanguagesSwich } from '../LanguagesSwich';
 import { languages } from '../language';
 import { useState, FC, useEffect } from 'react';
 import { ReactComponent as ActiveArrow } from '../../../assets/Header/activeArrow.svg';
 import { ReactComponent as PassiveArrow } from '../../../assets/Header/passiveArrow.svg';
 
 export const LanguageSelect = () => {
-	const [activeMenu, setActiveMenu] = useState<boolean>(false);
-	const [selectLang, setselectLang] = useState<string>('English');
+	const [activeMenu, setActiveMenu] = useState(false);
+	const [selectLang, setselectLang] = useState('English');
 
 	const handleActiveMenu = () => {
-		setActiveMenu((prevState) => !prevState);
+		setActiveMenu(!activeMenu);
 	};
-  const switchLang = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-    const lang = event.currentTarget.textContent;
-    setselectLang(lang || 'English');
-  };
+	const switchLang = (event: React.MouseEvent) => {
+		const lang = event.currentTarget.textContent;
+		
+		setselectLang(lang || 'English');
+	};
 
 	return (
-		<div onClick={handleActiveMenu} className={styles.LanguageSelect}>
+		<div onClick={handleActiveMenu} className={styles.languageSelect}>
 			<p className={styles.selectLanguage}>{selectLang}</p>
 			{activeMenu ? <PassiveArrow /> : <ActiveArrow />}
 			{activeMenu && (
-				<div className={styles.LanguagesBox}>
-					{languages.map((language, index) => (
-						<ul className={styles.LanguagesList}>
-							<li key={index} className={styles.LanguagesItem} 
-							onClick={switchLang}
-							>
-								{language.lang}
-							</li>
-						</ul>
-					))}
+				<div className={styles.languagesBox}>
+					<ul className={styles.languagesList}>
+						{languages.map((language) => (
+							<div className={styles.languagesItem_box} >
+								<li
+									key={language}
+									className={styles.languagesItem}
+									onClick={switchLang}
+								>
+									{language}
+								</li>
+								<span className={styles.language}>{language}</span>
+							</div>
+						))}
+					</ul>
 				</div>
 			)}
 		</div>
