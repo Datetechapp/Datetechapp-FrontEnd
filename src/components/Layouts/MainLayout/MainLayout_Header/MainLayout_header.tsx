490672da6656f17a6657e583eff3ada2530092e9
daryapatsko <daryapatsko95@gmail.com>
2024-01-22 09:40:11 +0300

    fix: return old version of MainLayout_header

--- src/components/Layouts/MainLayout/MainLayout_Header/MainLayout_header.tsx
+++ src/components/Layouts/MainLayout/MainLayout_Header/MainLayout_header.tsx
@@ -1,40 +1,20 @@
 import { Button } from 'components/common';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import bell from '../../../../assets/Header/bell-alert.svg';
 import logo from '../../../../assets/ModalAuth/logo.svg';
 import './index.css';
-import ModalNotification from 'components/ModalNotification/ModalNotification';
-import { notifications } from 'components/ModalNotification/notification';
-import { useNavigate } from 'react-router-dom';
 
 export const MainLayoutHeader: FC = () => {
-	const [isModalVisible, setIsModalVisible] = useState(false);
-	const navigate = useNavigate() ;
-
-	const handleOpenModal = () => {
-		setIsModalVisible(true);
-	};
-
-	const handleCloseModal = () => {
-		setIsModalVisible(false);
-	};
 
 	return (
 		<div className="header__wrapper">
-			<div className="logo">
-				<img src={logo} alt="logo" />
-			</div>
-			<div className="header__buttons">
-				<div className="notifications" 
-        onMouseEnter={handleOpenModal}
-        onClick={() => navigate('/notification')}
-        >
-					<div className="count_notifications">{notifications.length}</div>
-					<img src={bell} alt="notifications" />
-				</div>
-        {isModalVisible && <ModalNotification onClose={handleCloseModal} />}
-				<Button className="BtnUpload">Upload</Button>
-			</div>
+		  <div className="logo">
+			<img src={logo} alt="logo" />
+		  </div>
+		  <div className="header__buttons">
+			<img src={bell} alt="notifications" />
+			<Button className="BtnUpload">Upload</Button>
+		  </div>
 		</div>
-	);
+	  );
 };
