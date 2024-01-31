adfd368bb3e496082c14b89dc0802bcdefae1516
daryapatsko <daryapatsko95@gmail.com>
2024-01-31 14:53:27 +0300

    feat: added table with data of payment

--- src/components/Layouts/PaymentPageLayout/components/PaymentHistory/HistoryList/HistoryList.tsx
+++ src/components/Layouts/PaymentPageLayout/components/PaymentHistory/HistoryList/HistoryList.tsx
@@ -1,19 +1,69 @@
 import style from './HistoryList.module.css';
 import { payment } from '../payment';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
 export const HistoryList = () => {
+  const getRowClass = (status: string) => {
+    if (status === 'paid') {
+      return style.rowPaid;
+    } else if (status === 'progress') {
+      return style.rowProgress;
+    }
+
+    return style.rowCancel;
+  };
+
   return (
     <div className={style.boxHistory}>
       {payment.length === 0 ? (
         <p className={style.textHistory}>You do not have paid subscriptions</p>
       ) : (
-        <ul className={style.listHistory}>
-          {payment.map((item) => (
-            <li key={item.id} className={style.itemHistory}>
-              {item.id}
-            </li>
-          ))}
-        </ul>
+        <TableContainer
+          className={style.tableContainer}
+          sx={{
+            backgroundColor: '#201B27',
+            border: '2px solid #C896EF',
+            borderRadius: '10px',
+          }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow className={style.tableHeader}>
+                <TableCell>Data</TableCell>
+                <TableCell>Id</TableCell>
+                <TableCell>Subscription</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {payment.map((row) => (
+                <TableRow key={row.id} className={style.rowTitle}>
+                  <TableCell>{row.data}</TableCell>
+                  <TableCell>{row.id}</TableCell>
+                  <TableCell>{row.time}</TableCell>
+                  <TableCell>-{row.price}</TableCell>
+                  <TableCell>
+                    <div
+                      className={`${style.rowStatus} ${getRowClass(
+                        row.status,
+                      )}`}
+                    >
+                      {row.status}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
     </div>
   );
--- src/components/Layouts/PaymentPageLayout/components/PaymentHistory/payment.ts
+++ src/components/Layouts/PaymentPageLayout/components/PaymentHistory/payment.ts
@@ -25,20 +25,20 @@ export const payment = [
     id: '#15469987',
     time: '1 day subscription',
     price: '3.49€',
-    status: 'ok',
+    status: 'paid',
   },
   {
     data: '29.12.2023, 10:00',
     id: '#15469987',
     time: '1 day subscription',
     price: '3.49€',
-    status: 'ok',
+    status: 'paid',
   },
   {
     data: '29.12.2023, 10:00',
     id: '#15469987',
     time: '1 day subscription',
     price: '3.49€',
-    status: 'ok',
+    status: 'paid',
   },
 ];
