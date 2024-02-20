86a1c0a68ae82752443b0a8b916b4e4cb9098f6c
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-02-20 14:00:53 +0300

    feat: add add all necessary components

---
+++ src/components/Layouts/MessangerLayout/MessangerLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom';
+import style from './messangerLayout.module.css';
+
+import { EventsMessagesBlock } from 'components/EventsMessagesBlock';
+import { MainLayoutHeader } from '../MainLayout/MainLayout_Header/MainLayout_header';
+
+export const MessangerLayout = () => {
+  return (
+    <div className={style.container}>
+      <header className="mainLayoutHeader">
+        <MainLayoutHeader />
+      </header>
+      <section className={style.wrapper}>
+        <div className={style.leftBar}>
+          <EventsMessagesBlock />
+        </div>
+        <div className={style.mainContent}>
+          <Outlet />
+        </div>
+      </section>
+    </div>
+  );
+};
