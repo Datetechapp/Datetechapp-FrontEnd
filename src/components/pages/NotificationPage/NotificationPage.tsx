288c117d8a69f55cd8dce8a610252271aebc7925
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-02-28 20:43:11 +0300

    feat: add basic te4mplate of notification page

--- src/components/pages/NotificationPage/NotificationPage.tsx
+++ src/components/pages/NotificationPage/NotificationPage.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import { MainLayoutHeader } from 'components/Layouts/MainLayout/MainLayout_Header/MainLayout_header';
 import style from './Notification.module.css';
 import NotificationList from 'components/ModalNotification/NotificationList/NotificationList';
 
 const NotificationPage = () => {
   return (
     <div className={style.pageContainer}>
-      <h2 className={style.pageTitle}>Notifications</h2>
-      <div className={style.pageBox}>
-        <NotificationList />
-      </div>
+      <MainLayoutHeader />
+      <section className={style.pageWrapper}>
+        <h2 className={style.pageTitle}>Notifications</h2>
+        <div className={style.pageBox}>
+          <NotificationList />
+        </div>
+      </section>
     </div>
   );
 };
