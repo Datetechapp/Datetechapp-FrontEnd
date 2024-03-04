819e3bb0d287f50c66f4af650f0dd14946b19785
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-03-04 16:06:01 +0300

    feat: add new logo and language item into quistioners header

--- src/components/pages/Questionnaire/HeaderSecondary/HeaderSecondary.tsx
+++ src/components/pages/Questionnaire/HeaderSecondary/HeaderSecondary.tsx
@@ -1,19 +1,39 @@
-import { LanguageSelect } from 'components/Header/LanguageSelect/LanguageSelect';
 import { Button } from 'components/common';
-import { FC } from 'react';
-import { ReactComponent as Logo } from '../../../../assets/Header/logoForSecondHeader.svg';
+import { FC, useState } from 'react';
+import { ReactComponent as Logo } from '../../../../assets/ModalAuth/logo.svg';
 import css from './headerSecondary.module.css';
+import { LanguageSelect } from 'components/Header/LanguageSelect/LanguageSelect';
+import { useAppSelector } from 'hooks/hooks';
+import { getLanguage } from 'store/language/selectors';
+import { ReactComponent as ActiveArrow } from '../../../../assets/Header/activeArrow.svg';
+import { ReactComponent as PassiveArrow } from '../../../../assets/Header/passiveArrow.svg';
 
 interface HeaderSecondaryProps {
   text: string;
 }
 
 export const HeaderSecondary: FC<HeaderSecondaryProps> = ({ text }) => {
+  const [isActiveMenu, setIsActiveMenu] = useState(false);
+  const chosenlanguage = useAppSelector(getLanguage);
+
+  const handleActiveMenu = () => {
+    setIsActiveMenu(!isActiveMenu);
+  };
+
   return (
     <div className={css.header}>
       <Logo />
       <div className={css.languageButtonBlock}>
-        <LanguageSelect />
+        <div className={css.languageSelect} onClick={handleActiveMenu}>
+          <p className={css.selectLanguage}>{chosenlanguage}</p>
+          {isActiveMenu ? <PassiveArrow /> : <ActiveArrow />}
+          <div className={css.languagesContainer}>
+            <LanguageSelect
+              isActiveMenu={isActiveMenu}
+              setIsActiveMenu={setIsActiveMenu}
+            />
+          </div>
+        </div>
         <Button className={css.logOutBtn}>{text}</Button>
       </div>
     </div>
