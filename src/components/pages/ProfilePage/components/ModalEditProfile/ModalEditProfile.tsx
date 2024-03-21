1977d6514eedf0a07370903569b9b3918ba4dec7
halinapp <galunikipp@gmail.com>
2024-03-21 22:54:12 +0300

    feat: updated form validation, saved location data

--- src/components/pages/ProfilePage/components/ModalEditProfile/ModalEditProfile.tsx
+++ src/components/pages/ProfilePage/components/ModalEditProfile/ModalEditProfile.tsx
@@ -18,9 +18,22 @@ interface Values {
   about: string;
 }
 
+type SetFieldFunction = (field: string, value: string) => void;
+
+const mockValues = {
+  name: 'Nike Borzov',
+  day: '15',
+  month: '7',
+  year: '1980',
+  location: 'San Francisco, Argentina',
+  about: 'I am little horse',
+};
+
 const MyInput = ({ field, form, ...props }) => {
   return <Input {...field} {...props} />;
 };
+
+const months = monthsForLocale();
 
 export function ModalEditProfile({
   isOpen,
@@ -29,130 +42,153 @@ export function ModalEditProfile({
   isOpen: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }) {
-  const [location, setLocation] = useState('');
-  const [photo, setPhoto] = useState<string | null>(null);
-  const [selectedMonth, setSelectedMonth] = useState('1');
+  const [photo, setPhoto] = useState<File | null>(null);
+  const [location, setLocation] = useState(mockValues.location);
+  const [selectedMonth, setSelectedMonth] = useState(mockValues.month);
+
   const handleChangeOpen = () => {
     setOpen(!open);
   };
-  const months = monthsForLocale();
 
-  const handleClose = () => {
-    setOpen(false);
+  const handlePhotoUpload = (file: File | null) => setPhoto(file);
+
+  const handleCustomFieldChange = (
+    field: string,
+    value: string,
+    setFieldValue: SetFieldFunction,
+    setCustomField: (value: SetStateAction<string>) => void,
+  ) => {
+    setFieldValue(field, value);
+    setCustomField(value);
   };
 
-  const handlePhotoUpload = (file: string | null) => setPhoto(file);
+  const handleSubmit = (values: Values) => {
+    const profileInfo = { ...values, photo };
+
+    console.log('val=', profileInfo);
+  };
 
   return (
-    <>
-      <ModalBox
-        open={isOpen}
-        handleChangeOpen={handleChangeOpen}
-        maxWidth="560px"
-      >
-        <div className={styles.profileInfo}>
-          <div className={styles.header}>
-            <h1>Edit profile</h1>
-            <img
-              src={SimpleCloseIcon}
-              alt="SimpleCloseIcon"
-              style={{
-                cursor: 'pointer',
-              }}
-              onClick={handleChangeOpen}
-            />
-          </div>
-          <div className={styles.photo}>
-            <PhotoUploader onUpload={handlePhotoUpload} photo={photo || ''} />
-          </div>
-          <Formik
-            initialValues={{
-              name: '',
-              day: '',
-              year: '',
-              location: '',
-              about: '',
+    <ModalBox
+      open={isOpen}
+      handleChangeOpen={handleChangeOpen}
+      maxWidth="560px"
+    >
+      <div className={styles.profileInfo}>
+        <div className={styles.header}>
+          <h1>Edit profile</h1>
+          <img
+            src={SimpleCloseIcon}
+            alt="SimpleCloseIcon"
+            style={{
+              cursor: 'pointer',
             }}
-            validationSchema={profileSchema}
-            onSubmit={(values: Values) => {
-              console.log('submit', values);
-              console.log('selected', selectedMonth);
-            }}
-          >
-            {({ errors, touched }) => (
-              <Form className={styles.form}>
-                <div className={styles.twoCols}>
-                  <label>
-                    Name*
-                    <Field
-                      name="name"
-                      placeholder="Your name"
-                      component={MyInput}
-                    />
-                    {errors.name && touched.name ? (
-                      <div className={styles.error}>{errors.name}</div>
-                    ) : null}
-                  </label>
-
-                  <label>
-                    Date of Birth*
-                    <div className={styles.birthDay}>
-                      <Field
-                        name="day"
-                        component={MyInput}
-                        className={styles.day}
-                      />
-                      <Field
-                        className={styles.month}
-                        component={() =>
-                          Dropdown({
-                            selected: selectedMonth,
-                            options: months,
-                            onChange: setSelectedMonth,
-                          })
-                        }
-                      />
-                      <Field name="year" component={MyInput} />
-                    </div>
-                    {errors.day && touched.day ? (
-                      <div className={styles.error}>{errors.day}</div>
-                    ) : null}
-                    {errors.year && touched.year ? (
-                      <div className={styles.error}>{errors.year}</div>
-                    ) : null}
-                  </label>
-                </div>
-                <div className={styles.location}>
-                  <label>
-                    Location*
-                    <Field
-                      name="location"
-                      placeholder="Your location"
-                      component={() => Locations({ setLocation, location })}
-                    />
-                  </label>
-                  {errors.location && touched.location ? (
-                    <div className={styles.error}>{errors.location}</div>
-                  ) : null}
-                </div>
-                <div>
-                  <label>
-                    About me
-                    <Field
-                      name="about"
-                      placeholder="What would you like to tell us about yourself?"
-                      component={MyInput}
-                    />
-                  </label>
-                </div>
-                <button type="submit" className={styles.saveBtn}>
-                  Save
-                </button>
-              </Form>
-            )}
-          </Formik>
+            onClick={handleChangeOpen}
+          />
         </div>
-      </ModalBox>
-    </>
+        <div className={styles.photo}>
+          <PhotoUploader onUpload={handlePhotoUpload} photo={photo || null} />
+        </div>
+        <Formik
+          initialValues={mockValues}
+          validationSchema={profileSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isValid, errors, touched, setFieldValue }) => (
+            <Form className={styles.form}>
+              <div className={styles.twoCols}>
+                <label>
+                  Name*
+                  <Field
+                    name="name"
+                    placeholder="Your name"
+                    component={MyInput}
+                  />
+                  {errors.name && touched.name ? (
+                    <div className={styles.error}>{errors.name}</div>
+                  ) : null}
+                </label>
+
+                <label>
+                  Date of Birth*
+                  <div className={styles.birthDay}>
+                    <Field
+                      name="day"
+                      component={MyInput}
+                      className={styles.day}
+                    />
+                    <Field
+                      name="month"
+                      className={styles.month}
+                      component={() =>
+                        Dropdown({
+                          selected: selectedMonth,
+                          options: months,
+                          onChange: (value) =>
+                            handleCustomFieldChange(
+                              'month',
+                              value,
+                              setFieldValue,
+                              setSelectedMonth,
+                            ),
+                        })
+                      }
+                    />
+                    <Field name="year" component={MyInput} />
+                  </div>
+                  {errors.day && touched.day ? (
+                    <div className={styles.error}>{errors.day}</div>
+                  ) : null}
+                  {errors.year && touched.year ? (
+                    <div className={styles.error}>{errors.year}</div>
+                  ) : null}
+                </label>
+              </div>
+              <div className={styles.location}>
+                <label>
+                  Location*
+                  <Field
+                    name="location"
+                    placeholder="Your location"
+                    component={() =>
+                      Locations({
+                        setLocation: (value) =>
+                          handleCustomFieldChange(
+                            'location',
+                            value.toString(),
+                            setFieldValue,
+                            setLocation,
+                          ),
+                        location,
+                      })
+                    }
+                  />
+                </label>
+                {errors.location && touched.location ? (
+                  <div className={styles.error}>{errors.location}</div>
+                ) : null}
+              </div>
+              <div>
+                <label>
+                  About me
+                  <Field
+                    name="about"
+                    placeholder="What would you like to tell us about yourself?"
+                    component={MyInput}
+                  />
+                </label>
+              </div>
+              <button
+                type="submit"
+                className={styles.saveBtn}
+                disabled={!isValid}
+              >
+                Save
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </ModalBox>
   );
 }
--- src/components/pages/ProfilePage/components/ModalEditProfile/validate.schema.ts
+++ src/components/pages/ProfilePage/components/ModalEditProfile/validate.schema.ts
@@ -1,14 +1,43 @@
 import * as Yup from 'yup';
+import moment from 'moment';
+
+const minDayNumber = 1;
+const maxDayNumber = 31;
+const minAge = 18;
+const minNameLength = 2;
+const maxNameLength = 50;
 
 export const profileSchema = Yup.object().shape({
   name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .matches(/^[a-zA-Z]+$/, 'The name must contain only letters')
+    .min(minNameLength, 'Too Short!')
+    .max(maxNameLength, 'Too Long!')
+    .matches(/^[a-zA-Z ]+$/, 'The name must contain only letters')
     .required('Required name'),
-  location: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!'),
-  //   .required('Required loc'),
-  day: Yup.number().min(1).max(31).required('Required'),
-  year: Yup.number().min(1950).required('Required'),
+  day: Yup.number()
+    .min(minDayNumber)
+    .max(maxDayNumber)
+    .required('Required')
+    .test('is-valid-date', 'This is not valid day number', (value, context) => {
+      const { year, month } = context.parent;
+      const isValidDate = moment([year, month - 1, value]).isValid();
+
+      return isValidDate;
+    }),
+  month: Yup.number(),
+  year: Yup.string().test(
+    'is-valid-age',
+    `You can't be under ${minAge}`,
+    (value, context) => {
+      const { day, month } = context.parent;
+      const birthday = new Date(
+        Date.UTC(Number(value), month - 1, day, 0, 0, 0),
+      ).valueOf();
+      const now = new Date().valueOf();
+
+      const age = Math.abs(moment.duration(birthday - now).years());
+
+      return age >= minAge;
+    },
+  ),
   about: Yup.string(),
 });
